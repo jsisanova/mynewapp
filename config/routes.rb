efc2b8e8d9f4d1dc63840d3b9a3b400fbe688c40
jsisanova <jana.sisanova@gmail.com>
2017-08-25 16:02:08 +0200

    4.3 add orders to database

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-  root 'static_pages#index'
+  # root 'static_pages#index'
+
+  root 'static_pages#landing_page'
+
+  resources :orders, only: [:index, :show, :create, :destroy]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
