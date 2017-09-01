f173c8b7e77607f60b523842e29d6ece09b729aa
jsisanova <jana.sisanova@gmail.com>
2017-09-02 01:24:50 +0200

    5.3 send emails dev-mode + from heroku

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
   get 'static_pages/index'
   get 'static_pages/landing_page'
+  get 'static_pages/thank_you'
+
+  post 'static_pages/thank_you'
 
   root 'static_pages#landing_page'
 
