1e8e3126a8416c48e6689fefd0b9a29d45d1278a
jsisanova <jana.sisanova@gmail.com>
2017-09-01 20:31:30 +0200

    5.2 implement search box + product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,12 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    if params[:q]
+      search_term = params[:q]
+      @products = Product.search(search_term) # return our filtered list here
+    else
+      @products = Product.all
+    end # search box
   end
 
   # GET /products/1
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+
+  def self.search(search_term)
+    like_operator = Rails.env.production? ? "ilike" : "LIKE"
+		Product.where("name #{like_operator} ?", "%#{search_term}%")
+  end   # search box
+
+
 end
