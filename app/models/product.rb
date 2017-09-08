f8de2e414118a864abcaef09c7a80380f2027f83
jsisanova <jana.sisanova@gmail.com>
2017-09-08 22:25:26 +0200

    5.7 Create comments model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,19 @@
 class Product < ActiveRecord::Base
   has_many :orders
-  
+  has_many :comments
+
   def self.search(search_term)
     like_operator = Rails.env.production? ? "ilike" : "LIKE"
 		Product.where("name #{like_operator} ?", "%#{search_term}%")
   end   # search box
 
 
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_asc.first
+    # or comments.order(:rating)
+  end
 end
