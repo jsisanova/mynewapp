0d0634ad928cce49c17ff505a6a2b5c14f80ce05
jsisanova <jana.sisanova@gmail.com>
2017-09-09 12:51:17 +0200

    5.10 Add admin role and moderate user comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,12 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    # can :manage, User, id: user.id     before admin was created
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, User, id: user.id
+      cannot :destroy, :all
+    end
   end
 end
