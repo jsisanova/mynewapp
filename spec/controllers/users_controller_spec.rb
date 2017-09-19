c1a1833090bc2eb8605e2d76a3974e69e9157efc
jsisanova <jana.sisanova@gmail.com>
2017-09-19 22:01:52 +0200

    6.3 Add functional test to users

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe UsersController, type: :controller do
   let(:user) { User.create!(email: "hana9090@hotmail.com", password: "123456") }
-  # let(:user2) { User.create!(email: "test9090@hotmail.com", password: "123456") }
+  let(:user2) { User.create!(email: "test9090@hotmail.com", password: "123456") }
 
   describe 'GET #show' do
     context 'when a user is logged in' do
@@ -10,22 +10,22 @@ describe UsersController, type: :controller do
         sign_in user
       end
       it 'loads correct user details' do
-        get :show, params: { id: user.id }
-        # expect(response).to be_ok
+        get :show, id: user.id
         expect(assigns(:user)).to eq user
         expect(response).to have_http_status(200)
+        # expect(response).to be_ok # the same as status 200
+      end
+
+      it 'cant access other users show page' do
+        get :show, id: user2.id
+        expect(response).to redirect_to(root_path)
+        expect(response).to have_http_status(302)
       end
     end
-    #   it 'cant access other users show page' do
-    #     get :show, params: { id: user2.id }
-    #     expect(response).to have_http_status(302)
-    #     expect(response).to redirect_to(root_path)
-    #   end
-    # end
 
     context 'when a user is not logged in' do
       it 'redirects to login' do
-        get :show, params: { id: user.id }
+        get :show, id: user.id
         expect(response).to redirect_to(new_user_session_path)
       end
     end
