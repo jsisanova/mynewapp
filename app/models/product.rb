d351c09388b67a9aea2d97d3a849ad2795f343e5
jsisanova <jana.sisanova@gmail.com>
2017-10-02 14:11:12 +0200

    6.12 Add Redis and performance strategy

--- app/models/product.rb
+++ app/models/product.rb
@@ -22,4 +22,12 @@ class Product < ActiveRecord::Base
   def average_rating
   comments.average(:rating).to_f
   end
+
+  def views
+    $redis.get("product:#{id}")
+  end
+
+  def viewed!
+    $redis.incr("product:#{id}")
+  end
 end
