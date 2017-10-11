dd2bc52cad6f76079b37d651d7881f5f96e96b66
jsisanova <jana.sisanova@gmail.com>
2017-10-11 16:34:32 +0200

    6.15 Final changes

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -11,7 +11,7 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
     // Called when there's incoming data on the websocket for this channel
   $(".alert.alert-info").show();
 
-  $('.product-reviews').prepend(data.comment);
+  $('.new-product-review').prepend(data.comment);
   $("#average-rating").attr('data-score', data.average_rating);
   refreshRating();
 
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,9 @@
 if Rails.env.production?
   Rails.configuration.stripe = {
-    publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-    secret_key: ENV['STRIPE_SECRET_KEY']
+    # publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+    # secret_key: ENV['STRIPE_SECRET_KEY']     # env mode, not test mode
+    publishable_key: 'pk_test_bhei1ECcFWGnkm7ZcDXT1Kuz',
+    secret_key: 'sk_test_QnEc5CgPoyxev61QdYflN686'
   }
 else
   Rails.configuration.stripe = {
